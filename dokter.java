9e507c839a06644fe0cb5e849b9f98ddd1cccd5b
meylaniic <meylaniic@gmail.com>
2025-06-06 11:26:23 +0700

    first commit

---
+++ dokter.java
@@ -0,0 +1,9 @@
+public class dokter {
+    String idDokter;
+    String namaDokter;
+
+    public dokter(String idDokter, String namaDokter) {
+        this.idDokter = idDokter;
+        this.namaDokter = namaDokter;
+    }   
+}
---
+++ pasien.java
@@ -0,0 +1,18 @@
+public class pasien {
+    String nama;
+    String nik;
+    String keluhan;
+
+
+    public pasien(String nama, String nik, String keluhan) {
+        this.nama = nama;
+        this.nik = nik;
+        this.keluhan = keluhan;
+    }
+
+    public void tampilkanInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIK: " + nik);
+        System.out.println("Keluhan: " + keluhan);
+    }
+}
